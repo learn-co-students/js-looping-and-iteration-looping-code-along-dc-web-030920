b53c53677b42e28755f2e9f0ee1f3bdb62d6815f
rasdinho <ras.dinho7@gmail.com>
2020-04-20 00:32:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+// function writeCards(name, event){
+
+//     for (let i = 0; i < writeCards.length; i++){
+//         console.log(`Thank you, ${writeCards[i]}, for the wonderful birthday gift!`)
+//     }
+//     return writeCards;
+// }
+
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+
+  function countDown(number) {
+    while ( number > 0 ) {
+      console.log( number);
+      number -= 1;
+    }
+    console.log( number );
+  }
