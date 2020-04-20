7afb6e618d43f2c31bb3eb615325ef5524f0d6a7
bret-gibson <bret.gibson@gmail.com>
2020-04-20 04:42:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(array, str){
+    let newArr = [];
+
+    for (let i = 0; i < array.length; i++) {
+        newArr.push(`Thank you, ${array[i]}, for the wonderful ${str} gift!`);
+    }
+    
+    return newArr;
+}
+
+
+function countDown(num){
+    let counter = 0;
+    while (counter <= num ){
+        console.log(num);
+        num--;
+    }
+}
