1828e0bb1f1251ecc9374eeb1814835eda1cbf4c
tzipz23 <tomazyeanderson@gmail.com>
2020-04-19 20:31:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function writeCards(array, event) {
+    for (let i = 0; i < array.length; i++) {
+        array[i] = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+    }
+    return array
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
